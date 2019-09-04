306f714c21071d4d04300608200ce9f916c3c174
liutao <271297773@qq.com>
2019-09-05 00:16:11 +0800

    saas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello','webpack'],' ');
+    
+    element.classList.add('hello');
+
+    let myIcon = new Image();
+
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 }
