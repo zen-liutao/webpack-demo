1069baf15a43a3b17acd5e722f0415421897a9c3
liutao <271297773@qq.com>
2019-09-05 00:19:10 +0800

    remove config

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
+
 
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello','webpack'],' ');
     
-    element.classList.add('hello');
-
-    let myIcon = new Image();
-
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
+   
 
     return element;
 }
